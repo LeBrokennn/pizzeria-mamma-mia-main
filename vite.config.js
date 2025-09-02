199607c6d1bcb3b6a3a8904c267fef287feb281c
Javiera Saavedra Perez <sg.jjavisaavedra@gmail.com>
2025-09-01 23:18:52 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pizzeria-mamma-mia/', // repo de github pages
   plugins: [react()],
+  base: '/pizzeria-mamma-mia/', // repo de github pages
 });
 
