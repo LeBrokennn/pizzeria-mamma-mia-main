2a12dc47af0585a9db7c00c50d84b0fdbe8409d3
Javiera Saavedra Perez <sg.jjavisaavedra@gmail.com>
2025-09-08 22:59:38 -0300

    Integración Login y Register en la misma página

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,11 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-
-import App from './App.jsx'
-import LoginPage from './components/LoginPage.jsx'
-import RegisterPage from './components/RegisterPage.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </HashRouter>
+    <App />
   </React.StrictMode>
-)
+);
